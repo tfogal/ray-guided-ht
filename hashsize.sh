812b1f3e442ab0a5130fa2adefc5a2ddd5fd898b
Tom Fogal <tfogal@alumni.unh.edu>
2013-08-10 00:08:54 +0200

    fixed up the HT script.
    First of all, it's actually slow now, so I do a much sparser sampling.
    Secondly, I check for kernel launch failures and don't record data in
    that case; this will happen with small HTs, which is why we now start
    at 25 instead of 1; values lower than 25 fail *often*; a 1-element HT
    seems to fail always.
    'fail' is a misnomer here; we hit a timeout.  Maybe there's a way
    to increase the timeout?

--- hashsize.sh
+++ hashsize.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
 
-hsh="./Hash -X 64 -Y 64 -Z 60 -l 6 -r requests.data -b 4"
+hsh="./Hash -X 64 -Y 64 -Z 60 -l 6 -r requests.data -b 8"
 
 export COMPUTE_PROFILE=1
 export COMPUTE_PROFILE_LOG=./.ht.log
-echo "# ht-size GPU-Time Occupancy" > ht-size-naive.data
-echo "# ht-size GPU-Time Occupancy" > ht-size-staging.data
-for htsize in $(seq 1 150) ; do
+export NAIVE="ht-size-naive.data"
+export STAGING="ht-size-staging.data"
+echo "# ht-size GPU-Time Occupancy" > ${NAIVE}
+echo "# ht-size GPU-Time Occupancy" > ${STAGING}
+for htsize in $(seq 25 5 150) ; do
 	# There seems to be an issue with the first run; I guess it takes a sec
 	# for the GPU to get into "CUDA mode".  So run once unrecorded, to
 	# prime CUDA.
 	${hsh} -n -h ${htsize} &>/dev/null
 	${hsh} -h ${htsize} &>/dev/null
 	for i in 1 2 3 ; do
+		# make sure we don't get nonsense.
+		rm -f ${COMPUTE_PROFILE_LOG}
                 ${hsh} -n -h ${htsize} &>/dev/null
-                awk -S --assign variable="${htsize}" \
-			-f parse-profile.awk .ht.log >> ht-size-naive.data
+		if test $? -eq 0; then
+			awk -S --assign variable="${htsize}" \
+				-f parse-profile.awk \
+				${COMPUTE_PROFILE_LOG} >> ${NAIVE}
+		fi
 
+		rm -f ${COMPUTE_PROFILE_LOG}
 		${hsh} -h ${htsize} &>/dev/null
-                awk -S --assign variable="${htsize}" \
-			-f parse-profile.awk .ht.log >> ht-size-staging.data
+		if test $? -eq 0; then
+			awk -S --assign variable="${htsize}" \
+				-f parse-profile.awk \
+				${COMPUTE_PROFILE_LOG} >> ${STAGING}
+		fi
 	done
 	progress=$(echo "(${htsize} / 150) * 100.00" | bc -l)
 	/bin/echo -en "\r${progress}..."
 done
 echo "" # newline so that the above echos don't steal the line.
+gnuplot size-ht.gnu
