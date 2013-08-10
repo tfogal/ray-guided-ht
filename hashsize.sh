48a78f921b3e5a2ef1a5ad8e287ed4648a28f7d2
Tom Fogal <tfogal@alumni.unh.edu>
2013-08-10 14:47:25 +0200

    High-definition hash size sampling.

--- hashsize.sh
+++ hashsize.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-hsh="./Hash -X 64 -Y 64 -Z 60 -l 6 -r requests.data -b 8"
+hsh="./Hash -X 64 -Y 64 -Z 60 -l 6 -r requests.req -b 2"
 
 export COMPUTE_PROFILE=1
 export COMPUTE_PROFILE_LOG=./.ht.log
 export NAIVE="ht-size-naive.data"
 export STAGING="ht-size-staging.data"
+export MAX_SIZE=200
 echo "# ht-size GPU-Time Occupancy" > ${NAIVE}
 echo "# ht-size GPU-Time Occupancy" > ${STAGING}
-for htsize in $(seq 25 5 150) ; do
+for htsize in $(seq 25 1 ${MAX_SIZE}) ; do
 	# There seems to be an issue with the first run; I guess it takes a sec
 	# for the GPU to get into "CUDA mode".  So run once unrecorded, to
 	# prime CUDA.
 	${hsh} -n -h ${htsize} &>/dev/null
 	${hsh} -h ${htsize} &>/dev/null
-	for i in 1 2 3 ; do
+	for i in 1 2 3 4 5; do
 		# make sure we don't get nonsense.
 		rm -f ${COMPUTE_PROFILE_LOG}
                 ${hsh} -n -h ${htsize} &>/dev/null
@@ -22,6 +23,8 @@ for htsize in $(seq 25 5 150) ; do
 			awk -S --assign variable="${htsize}" \
 				-f parse-profile.awk \
 				${COMPUTE_PROFILE_LOG} >> ${NAIVE}
+		else
+			echo "wtf?!"
 		fi
 
 		rm -f ${COMPUTE_PROFILE_LOG}
@@ -30,9 +33,11 @@ for htsize in $(seq 25 5 150) ; do
 			awk -S --assign variable="${htsize}" \
 				-f parse-profile.awk \
 				${COMPUTE_PROFILE_LOG} >> ${STAGING}
+		else
+			echo "WTF?!"
 		fi
 	done
-	progress=$(echo "(${htsize} / 150) * 100.00" | bc -l)
+	progress=$(echo "(${htsize} / ${MAX_SIZE}) * 100.00" | bc -l)
 	/bin/echo -en "\r${progress}..."
 done
 echo "" # newline so that the above echos don't steal the line.
